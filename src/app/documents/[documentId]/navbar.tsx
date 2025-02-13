cc1bd042398bddda970fb70547b993102071bed5
Dimitar <dimitar.mandarliev@gmail.com>
2025-02-13 14:45:36 +0200

    Populating user information

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -36,6 +36,7 @@ import {
 import { BsFilePdf } from "react-icons/bs";
 import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import { Avatars } from "./avatars";
 
 const Navbar = () => {
   const { editor } = useEditorStore();
@@ -262,6 +263,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="flex gap-3 items-center pl-6">
+        <Avatars />
         <OrganizationSwitcher
           afterCreateOrganizationUrl="/"
           afterLeaveOrganizationUrl="/"
